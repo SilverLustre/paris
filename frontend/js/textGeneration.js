c6c774c904a70dcd912bc0ee7442519f911e26fb
Jiahe Tian <jiahet@andrew.cmu.edu>
2022-06-23 20:01:35 +0930

    Add prompt, subtitles and generation result

---
+++ frontend/js/textGeneration.js
@@ -0,0 +1,24 @@
+var textTypeClrBt = document.getElementById("textTypeClrBt");
+textTypeClrBt.onclick = function(){
+    document.getElementById("textTypeInput").value = '';
+}
+
+var toneClrBt = document.getElementById("toneClrBt");
+toneClrBt.onclick = function(){
+    document.getElementById("toneInput").value = '';
+}
+
+var targetAudienceClrBt = document.getElementById("targetAudienceClrBt");
+targetAudienceClrBt.onclick = function(){
+    document.getElementById("targetAudienceInput").value = '';
+}
+
+var keywordsClrBt = document.getElementById("keywordsClrBt");
+keywordsClrBt.onclick = function(){
+    document.getElementById("keywordsInput").value = '';
+}
+
+var topicTextareaClrBt = document.getElementById("topicTextareaClrBt");
+topicTextareaClrBt.onclick = function(){
+    document.getElementById("topicTextarea").value = '';
+}
