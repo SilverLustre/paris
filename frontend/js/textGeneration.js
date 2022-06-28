29ea871552f352be83c50cfe7f491ee2f8b21092
Jiahe Tian <jiahet@andrew.cmu.edu>
2022-06-28 23:59:32 +0930

    Topic section moved up; Add prompt and subtitle generation logic;

--- frontend/js/textGeneration.js
+++ frontend/js/textGeneration.js
@@ -28,4 +28,122 @@ topicTextareaClrBt.onclick = function(){
 
 // Model Parameters Block
 
+var promptTextarea = document.getElementById("promptTextarea");
+
+
+function generatePrompt(){
+    console.log("generatePrompt");
+    
+    var numOfSubsInput = document.getElementById("numOfSubsInput").value;
+    var textType = document.getElementById("textTypeInput").value;
+    textType = textType===''?'article':textType;
+    var topic = document.getElementById("topicTextarea").value;
+    var tone = document.getElementById("toneInput").value;
+    var targetAudience = document.getElementById("targetAudienceInput").value;
+    var keywords = document.getElementById("keywordsInput").value;
+    
+
+    var elements = []
+    if (numOfSubsInput===''||numOfSubsInput===0||numOfSubsInput==='0'){
+        // without specified subtitles
+        elements = ['Write a'];
+        if (textType[0])
+        elements.push(textType);
+        elements.push('about');
+        elements.push(topic);
+        if (tone!==''){
+            elements.push('in');
+            elements.push(tone);
+            elements.push('tone');
+        }
+        if (targetAudience!==''){
+            elements.push('targeting the audience of');
+            elements.push(targetAudience);
+        }
+        if (keywords!==''){
+            elements.push('with keywords:');
+            elements.push(keywords);
+        }
+    }else{
+        // with specified subtitles
+        elements = ['Generate'];
+        elements.push(numOfSubsInput);
+        elements.push('subtitles of a');
+        elements.push(textType);
+        elements.push('about');
+        elements.push(topic);
+        if (tone!==''){
+            elements.push('in');
+            elements.push(tone);
+            elements.push('tone');
+        }
+        if (targetAudience!==''){
+            elements.push('targeting the audience of');
+            elements.push(targetAudience);
+        }
+        if (keywords!==''){
+            elements.push('with keywords:');
+            elements.push(keywords);
+        }
+    }
+
+    promptTextarea.value = elements.join(' ');
+}
+
+function generateSubtitles(){
+    if (numOfSubsInput===''||numOfSubsInput===0||numOfSubsInput==='0'){
+        alert('Number of subtitles must be set larger than 0.');
+        return;
+    }
+    var engine = document.getElementById('modelSelect').value;
+    var maxToken = document.getElementById('maxTokenInput').value;
+    var temperature = document.getElementById('tempInput').value;
+    var presencePenalty = document.getElementById('presencePenaltyInput').value;
+    var freqPenalty = document.getElementById('freqPenaltyInput').value;
+    var prompt = document.getElementById('promptTextarea').value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4){
+            // complete
+            if (this.status == 200){
+                console.log(this.responseText);
+            }
+
+        }
+    }
+    xhttp.open("POST","http://127.0.0.1:8000/translate");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    // xhttp.setRequestHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8000/");
+    xhttp.send(JSON.stringify({
+        "engine": engine,
+        "temperature": temperature,
+        "max_tokens": maxToken,
+        "frequency_penalty":freqPenalty,
+        "presence_penalty": presencePenalty,
+        "prompt": prompt
+    }));
+
+}
+
+function subtitlesToArticle(){
+    console.log("subtitlesToArticle");
+}
+
+
+
+var modelParamsGenPromptBt = document.getElementById("modelParamsGenPromptBt");
+modelParamsGenPromptBt.onclick = function(){
+    generatePrompt();
+}
+
+var modelParamsGenArticleBt = document.getElementById("modelParamsGenArticleBt");
+
+
+
 // Prompt Block
+
+var promptClrBt = document.getElementById("promptClrBt");
+promptClrBt.onclick = function(){
+    promptTextarea.value='';
+}
