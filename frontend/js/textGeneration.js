d8162bf1035240cd44f562024827f6b1827fb8cc
Jiahe Tian <jiahet@andrew.cmu.edu>
2022-07-05 21:35:39 +0930

    Subtitles generation

--- frontend/js/textGeneration.js
+++ frontend/js/textGeneration.js
@@ -1,38 +1,38 @@
 // Title Block
-function setServiceStatus(status){
-    var serviceStatus = document.getElementById("serviceStatus")
-    if (status === "Online"){
-        serviceStatus.innerHTML = status;
-        serviceStatus.classList.replace("red", "green");
-    }else if (status === "Offline"){
-        serviceStatus.innerHTML = status;
-        serviceStatus.classList.replace("green", "red");
-    }
-}
-window.setInterval(function(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if (this.readyState === 4){
-            if (this.status === 200){
-                // console.log(this.status);
-                // console.log(this.responseText);
-                setServiceStatus("Online");
-            }else{
-                // console.log(this.status);
-                // console.log(this.responseText);
-                setServiceStatus("Offline");
-            }
-        }
-    }
+// function setServiceStatus(status){
+//     var serviceStatus = document.getElementById("serviceStatus")
+//     if (status === "Online"){
+//         serviceStatus.innerHTML = status;
+//         serviceStatus.classList.replace("red", "green");
+//     }else if (status === "Offline"){
+//         serviceStatus.innerHTML = status;
+//         serviceStatus.classList.replace("green", "red");
+//     }
+// }
+// window.setInterval(function(){
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function(){
+//         if (this.readyState === 4){
+//             if (this.status === 200){
+//                 // console.log(this.status);
+//                 // console.log(this.responseText);
+//                 setServiceStatus("Online");
+//             }else{
+//                 // console.log(this.status);
+//                 // console.log(this.responseText);
+//                 setServiceStatus("Offline");
+//             }
+//         }
+//     }
 
-    xhttp.timeout = 8000;
-    xhttp.ontimeout = function(){
-        // console.log('Status check timed out.')
-    }
-    xhttp.open("GET", "http://127.0.0.1:8000/aloha");
-    xhttp.setRequestHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8000/");
-    xhttp.send(null, true);
-}, 10000);
+//     xhttp.timeout = 8000;
+//     xhttp.ontimeout = function(){
+//         // console.log('Status check timed out.')
+//     }
+//     xhttp.open("GET", "http://127.0.0.1:8000/aloha");
+//     xhttp.setRequestHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8000/");
+//     xhttp.send(null, true);
+// }, 10000);
 
 // Configurations Block
 var textTypeClrBt = document.getElementById("textTypeClrBt");
@@ -145,38 +145,55 @@ function generateSubtitles(){
     console.log('generateSubtitles');
     var subtitlesTextarea = document.getElementById('subtitlesTextarea');
     var numOfSubsInput = document.getElementById("numOfSubsInput").value;
-    if (numOfSubsInput===''||numOfSubsInput===0||numOfSubsInput==='0'){
-        alert('Number of subtitles must be set larger than 0.');
-        return;
-    }
     subtitlesTextarea.value = 'Generating...';
-    var engine = document.getElementById('modelSelect').value;
+    var model = document.getElementById('modelSelect').value;
     var maxToken = document.getElementById('maxTokenInput').value;
     var temperature = document.getElementById('tempInput').value;
     var presencePenalty = document.getElementById('presencePenaltyInput').value;
     var freqPenalty = document.getElementById('freqPenaltyInput').value;
     var prompt = document.getElementById('promptTextarea').value;
+    if (prompt.length==0){
+        alert('Prompt cannot be empty.');
+        return;
+    }
+    if ((numOfSubsInput===''||numOfSubsInput===0||numOfSubsInput==='0')&&prompt.length==0){
+        alert('Number of subtitles must be set larger than 0.');
+        return;
+    }
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if (this.readyState === 4){
             // complete
             if (this.status === 200){
-                console.log(this.responseText);
-                var jsonObj = JSON.parse(JSON.parse(this.responseText));
-                subtitlesTextarea.value = jsonObj.subtitles.join('\n');
+                var jsonObj = JSON.parse(this.responseText);
+                console.log(jsonObj);
+                // subtitlesTextarea.value = jsonObj.choices[0].text;
+                var reg = /\d+\.(.*)/
+                var subtitleStr = jsonObj.choices[0].text;
+                var subtitles = subtitleStr.split("\n");
+                var results = [];
+                for (let i = 0; i < subtitles.length; i++) {
+                    if (subtitles[i].length>0){
+                        var matchResult = subtitles[i].match(reg);
+                        if (matchResult!=null&&matchResult.length>=2){
+                            results.push(matchResult[1].trim());
+                        }
+                    }
+                }
+                subtitlesTextarea.value = results.join("\n");
             }
         }
     }
-    xhttp.open("POST","http://127.0.0.1:8000/subtitle");
+    xhttp.open("POST","https://api.openai.com/v1/completions");
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.setRequestHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8000/");
+    xhttp.setRequestHeader("Authorization", "Bearer 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");
     xhttp.send(JSON.stringify({
-        "engine": engine,
-        "temperature": temperature,
-        "max_tokens": maxToken,
-        "frequency_penalty":freqPenalty,
-        "presence_penalty": presencePenalty,
+        "model": model,
+        "temperature": parseFloat(temperature),
+        "max_tokens": parseInt(maxToken),
+        "frequency_penalty":parseFloat(freqPenalty),
+        "presence_penalty": parseFloat(presencePenalty),
         "prompt": prompt
     }),true);
 
